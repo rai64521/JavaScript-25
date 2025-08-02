254d90527978a3568ece34e9dbad0e8e88fe4a62
sumanttraii <sumanttrai99@gmail.com>
2025-08-02 17:24:31 +0530

    json

---
+++ 02_basics/Json.js
@@ -0,0 +1,39 @@
+const course = {
+    coursename: "JavaScript",
+    price: "999",
+    courseInstructor: "sumant"
+}
+
+// course.courseInstructor
+
+const {courseInstructor: instructor} = course
+
+console.log(instructor);
+
+
+
+
+// In React
+// const navbar = ({company}) => {
+
+// }
+
+// navbar(company = "sumant")
+
+
+
+// API 
+
+//JSON  => JavaScript Object Notation
+
+// {
+//     "name": "sumant",
+//     "coursename": "JavaScript",
+//     "price": "free"
+// }
+
+// [
+//     {},
+//     {},
+//     {}
+// ]

254e18af81edd788d523e8f6fd50227233386aee
sumanttraii <sumanttrai99@gmail.com>
2025-08-02 19:40:53 +0530

    iife in javascript

---
+++ 03_basics/05_iife.js
@@ -0,0 +1,13 @@
+// Immediately Invoked FUnction Expressions (IIFE)
+// ()()
+
+(function chai(){
+    console.log(`DB CONNECTED`);
+    
+})();
+
+( (name) => {
+    console.log(`DB CONNECTED TWO ${name}`);
+    
+})('sumant')
+

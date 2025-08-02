c0b405887319de145c4105b64c4fe97bc692b92b
sumanttraii <sumanttrai99@gmail.com>
2025-08-02 15:51:40 +0530

    array in javascript

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,49 @@
+// Array
+
+const myArr = [1, 2, 3, 4, 5]
+const myFriends = ["shubhangi", "Rishu", "Tripti", "Diya"]
+
+
+const myArr2 = new Array(2, 4, 6, 8, 10)
+
+
+
+// console.log(myArr[0]);
+
+
+// Array Methods
+
+// myArr.push(6)
+// myArr.push(8)
+// myArr.pop()
+
+// myArr.unshift(9)
+// myArr.shift()
+// myArr.shift()
+
+// console.log(myArr.includes(9));
+// console.log(myArr.indexOf(3));
+
+// const newArr = myArr.join()
+
+
+
+// console.log(myArr);
+// console.log(newArr);
+// console.log(typeof newArr);
+
+
+
+console.log("A ", myArr);
+const myn1 = myArr.slice(1, 3)
+
+console.log(myn1);
+console.log("B ",myArr);
+
+const myn2 = myArr.splice(1, 3)
+console.log(myn2);
+console.log("C ",myArr);
+
+
+
+

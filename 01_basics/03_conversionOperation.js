118097eaad13190a45288feefe23d29df48aea76
sumanttraii <sumanttrai99@gmail.com>
2025-08-02 12:24:01 +0530

    conversion od dataType

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,38 @@
+// let score = "33abc"
+// let score = null
+// let score = undefined
+// let score = true
+let score = "sumant"
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+
+let valueInNumber = Number(score)
+
+// console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+// console.log(typeof booleanIsLoggedIn);
+
+
+// 1 => true; 0 => false
+// "" => false
+// "sumant" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+
+console.log(typeof stringNumber);
+console.log(stringNumber);
+

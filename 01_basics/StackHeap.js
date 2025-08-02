6727bc4734c994211468ab499641bf0da9b3c0a2
sumanttraii <sumanttrai99@gmail.com>
2025-08-02 14:00:42 +0530

    stack and heap in js

---
+++ 01_basics/StackHeap.js
@@ -0,0 +1,24 @@
+// Stack (Primitive), Heap (Non-Primitive)
+
+let myYoutubeName = "sumantraidotcom"
+
+let anotherName = myYoutubeName
+anotherName = "rishurai"
+
+console.log(myYoutubeName);
+console.log(anotherName);
+
+
+const user = {
+    email: "user@google.com",
+    upi: "user@ybl"
+}
+
+const newUser = user
+newUser.email = "rai@microsoft.com"
+
+console.log(user.email);
+console.log(newUser.email);
+
+
+

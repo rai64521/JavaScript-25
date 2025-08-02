b4f5431b44742ca9711d5bb18dada3da1ef6ca8d
sumanttraii <sumanttrai99@gmail.com>
2025-08-02 16:18:27 +0530

    array in js

---
+++ 02_basics/02_arrays.js
@@ -0,0 +1,26 @@
+const marvel_heros = ["thor", "Ironman", "spiderman"]
+const dc_heros = ["superman", "flash", "batman"]
+
+// marvel_heros.push(dc_heros)
+// console.log(marvel_heros);
+// console.log(marvel_heros[3][1]);
+
+
+// const all_heros = marvel_heros.concat(dc_heros)
+
+// console.log(all_heros);
+
+const all_new_heros = [...marvel_heros, ...dc_heros]
+console.log(all_new_heros);
+
+
+const anotherArray = [1, 2, 3, 4, [5, 6, 7], [6, 7, [4, 5]]]
+
+const real_another_array = anotherArray.flat(Infinity)
+console.log(real_another_array);
+
+
+console.log(Array.isArray("Sumant"));
+console.log(Array.from("Sumant"));
+
+console.log(Array.from({name: "sumant"}));  // Interesting

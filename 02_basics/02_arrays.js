48ed0f649b3982162b32ec112711540c5396e36e
sumanttraii <sumanttrai99@gmail.com>
2025-08-02 16:20:12 +0530

    arrays in javascript

--- 02_basics/02_arrays.js
+++ 02_basics/02_arrays.js
@@ -24,3 +24,9 @@ console.log(Array.isArray("Sumant"));
 console.log(Array.from("Sumant"));
 
 console.log(Array.from({name: "sumant"}));  // Interesting
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+
+console.log(Array.of(score1,score2,score3));

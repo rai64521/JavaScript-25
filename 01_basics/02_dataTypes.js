aef67bc751e09cf4c21743b768dc18db18f6cfef
sumanttraii <sumanttrai99@gmail.com>
2025-08-02 12:09:01 +0530

    datattypes in javascript

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,34 @@
+"use strict";   // treat all JS code as newer version
+
+// alert("hello")    // we are using nodejs, not browser
+
+
+
+const name = "Sumant"        
+let age = 3
+let isLoggedIn = true
+let state;
+
+// number => 2 to power 53
+// bigint 
+// string => ""
+// boolean => true/false
+// null =>  standalone value
+// undefined => 
+// symbol  => unique
+
+
+
+// object
+
+
+// console.log(typeof "");
+// console.log(typeof age);
+
+console.log(typeof null);             // Object
+
+console.log(typeof undefined);       // Undefined
+
+
+
+

d8aa2ccb7072adbacb052fe09bc2084be27a006b
sumanttraii <sumanttrai99@gmail.com>
2025-08-02 19:03:59 +0530

    scope and hoisting in js

--- 03_basics/03_scope.js
+++ 03_basics/03_scope.js
@@ -10,6 +10,53 @@ if (true) {
    
 }
 
-console.log(a);
+// console.log(a);
 // console.log(b);
 // console.log(c);
+
+
+
+function one(){
+    const username = "sumant"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+        
+    }
+    // console.log(website);
+    two()
+    
+}
+
+// one()
+
+if(true){
+    const username = "sumant"
+    if(username === "sumant"){
+        const website = " youtube"
+        // console.log(username + website);
+        
+    }
+    // console.log(website);   // website is not access here it is out of scope  
+}
+// console.log(username);      // username is not accessible here it is out of scope
+
+
+// ++++++++++++++++++++++++++++++++++++++ nteresting ++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+console.log(addone(4));
+
+function addone(num){
+    return num + 1
+}
+
+
+// console.log(addone(4));
+
+addTwo(3)
+const addTwo = function(num){
+    return num + 2
+}
+

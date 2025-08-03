4b43d5bff0567b19af4e49d9acdb8786b55a151a
sumanttraii <sumanttrai99@gmail.com>
2025-08-03 11:47:53 +0530

    while and do while loop in js

---
+++ 05_iterations/two.js
@@ -0,0 +1,41 @@
+// while
+
+// while (condition) {
+
+// }
+
+let index = 0;
+
+while (index <= 10) {
+//   console.log(`value of index is ${index}`);
+  index = index + 2;
+}
+
+
+const myArr = ["Rajkumar", "prinsu", "yogesh", "anurag", "aniket", "aman", "shivansh"]
+let i = 0
+
+while (i < myArr.length) {
+
+    // console.log(`my friends name is ${myArr[i]}`);
+    i++
+     
+}
+
+
+
+
+// do while 
+
+// do {
+    
+// } while (condition);
+
+
+let score = 11
+
+do {
+    console.log(`score is ${score}`);
+    score ++
+    
+} while (score <= 10);
